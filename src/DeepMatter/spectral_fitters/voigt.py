24b619e399ed31c9dcb4486a9f50feb727617d25
Joshua C Agar <jca318@lehigh.edu>
2021-06-08 13:32:37 -0400

    update dataset_generator

--- src/DeepMatter/spectral_fitters/voigt.py
+++ src/DeepMatter/spectral_fitters/voigt.py
@@ -3,7 +3,7 @@ import math
 from ..rand_util.rand_gen import rand_tensor
 
 
-class Gaussian:
+class PsuedoVoigt:
     """
     Class that computes the gaussian
     """
@@ -12,6 +12,7 @@ class Gaussian:
                  sd=[0, 1],
                  mean=[0, 1],
                  amp=[0, 1],
+                 fraction=[0, 1],
                  size=(1, 1),
                  verbose=False):
         """
@@ -30,6 +31,7 @@ class Gaussian:
         self.mean = mean
         self.amp = amp
         self.x_vector = x_vector
+        self.fraction = fraction
         self.size = size
         self.verbose = verbose
 
@@ -48,7 +50,7 @@ class Gaussian:
             print({f'pre-param size {params.size()}'})
 
         if len(params.size()) == 2:
-            params = torch.reshape(params, (params.shape[0], 3, -1))
+            params = torch.reshape(params, (params.shape[0], 4, -1))
 
         if self.verbose:
             print(f'parm size = {params.size()}')
@@ -65,6 +67,7 @@ class Gaussian:
             _mean = params[:, 0, i].to(device)
             _sd = params[:, 1, i].to(device)
             _amp = params[:, 2, i].to(device)
+            _fraction = params[:, 3, i].to(device)
 
             x_vector = torch.cat(params.shape[0] * [self.x_vector]).reshape(
                 params.shape[0], -1).to(device)
@@ -74,13 +77,21 @@ class Gaussian:
             if self.verbose:
                 print(f'x_vector_shape = {x_vector.size()}')
 
-            _temp = torch.tensor(math.pi * 2)
-            _base = 1 / (_sd * (torch.sqrt(_temp)))
+            _log = torch.tensor(math.log(2))
+            _sigma = _sd / torch.sqrt(2 * _log)
+            _pi = torch.tensor(math.pi)
+            _temp = ((1 - _fraction) * _amp) / (_sigma * torch.sqrt(_pi * 2))
 
             if self.verbose:
                 print(f'mean_size = {_mean.size()}')
-            _exp = torch.exp(-.5 * ((torch.pow((torch.sub(x_vector, _mean)), 2)) / torch.pow(_sd, 2)))
-            _out = _amp * _base * _exp
+
+            _square = torch.pow((x_vector - _mean), 2)
+            _exp = torch.exp( (0 - _square) / (2 * torch.pow(_sigma,2)))
+            _left = _temp * _exp
+            _temp = (_fraction * _amp) / _pi
+            _right = _temp * (_sd / (_square + torch.pow(_sd, 2)))
+            _out = _left + _right
+
             if self.verbose:
                 print(f'amp {_amp.size()}, base {_base.size()}, exp {_exp.size()}')
                 print(f'out shape = {_out.shape}')
@@ -103,8 +114,9 @@ class Gaussian:
         sd = rand_tensor(min=self.sd[0], max=self.sd[1], size=self.size)
         mean = rand_tensor(min=self.mean[0], max=self.mean[1], size=self.size)
         amp = rand_tensor(min=self.amp[0], max=self.amp[1], size=self.size)
+        fraction = rand_tensor(min=self.self.fraction[0], max=self.self.fraction[1], size=self.size)
 
-        _params = torch.torch.stack((mean, sd, amp))
+        _params = torch.torch.stack((mean, sd, amp, fraction))
         _params = torch.atleast_2d(_params)
         _params = torch.transpose((_params), 0, 1)
         return self.compute(_params, device=device), _params
